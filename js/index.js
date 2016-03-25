5a66774b79c6ab42f1e22139e52ed4c2f09d3067
QxQstar <1395294694@qq.com>
2016-03-25 17:03:02 +0800

    更改了页面描述

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 		navigation: true,
 		navigationPosition: 'right',
 		scrollOverflow:true,
-		fixedElements: '#nav, #fixedImg',
+		fixedElements: '#nav',
 		onLeave:function(index,nextindex){
-			if(nextindex == 4||nextindex == 5){
-				hiddenFixedImg();
-			}
-			if(nextindex == 1||nextindex == 2||nextindex == 3){
-				hiddenP4Img();
-			}
+			// if(nextindex == 4||nextindex == 5){
+			// 	hiddenFixedImg();
+			// }
+			// if(nextindex == 1||nextindex == 2||nextindex == 3){
+			// 	hiddenP4Img();
+			// }
 			switch(index){
 				case 1:hiddendescription1();break;
 				case 2:hiddendescription2();break;
@@ -21,12 +21,12 @@ $(document).ready(function(){
 			}
 		},
 		afterLoad:function(link,index){
-			if(index == 4||index == 5){
-				occurP4Img();
-			}
-			if(index == 1||index == 2||index == 3){
-				occurFixedImg();
-			}
+			// if(index == 4||index == 5){
+			// 	occurP4Img();
+			// }
+			// if(index == 1||index == 2||index == 3){
+			// 	occurFixedImg();
+			// }
 			switch(index){
 				case 1: occurdescription1();hiddendescription();break;
 				case 2: occurdescription2();
@@ -45,87 +45,85 @@ $(document).ready(function(){
 		},
 	});
 });
-function hiddenFixedImg(){
-	$('#fixedImg').fadeOut(1000);
-}
-function hiddenP4Img(){
-	$('#page4Img').fadeOut(1000);
-}
-function occurFixedImg(){
-	$('#fixedImg').fadeIn(200);
-}
-function occurP4Img(){
-	$('#page4Img').fadeIn(1000);
-  }
+var span = document.getElementsByTagName('span');
+span[0].onclick = function(){
+	location.href='#Page2';
+};
+span[1].onclick = function(){
+	location.href='#Page3';
+};
+span[2].onclick = function(){
+	location.href='#Page4';
+};
+span[3].onclick = function(){
+	location.href='#Page5';
+};
+// function hiddenFixedImg(){
+// 	$('#fixedImg').fadeOut(1000);
+// }
+// function hiddenP4Img(){
+// 	$('#page4Img').fadeOut(1000);
+// }
+// function occurFixedImg(){
+// 	$('#fixedImg').fadeIn(200);
+// }
+// function occurP4Img(){
+// 	$('#page4Img').fadeIn(1000);
+//   }
 function hiddendescription1(){
-  $('#pOne').fadeOut(1000);
-  setTimeout('h1OneFadeOut()',100);
-  }
-function h1OneFadeOut(){
-	$('#h1One').fadeOut(1000);
+  $('#pOne').fadeOut();
+  $('#h1One').fadeOut();
   }
 function hiddendescription2(){
-  $('#pTwo').fadeOut(1000);
-	setTimeout('h1TwoFadeOut()',100);
-  }
-function h1TwoFadeOut(){
-  $('#h1Two').fadeOut(1000);
+  $('#pTwo').fadeOut();
+  $('#h1Two').fadeOut();
   }
 function hiddendescription3(){
-  $('#pThree').fadeOut(1000);
-  setTimeout('h1ThreeFadeOut()',100);
-  }
-function h1ThreeFadeOut(){
-	$('#h1Three').fadeOut(1000);
+  	$('#pThree').fadeOut();
+	$('#h1Three').fadeOut();
   }
 function hiddendescription4(){
-  $('#pFour').fadeOut(1000);
-  setTimeout('h1FourFadeOut()',100);
-  }
-function h1FourFadeOut(){
-	$('#h1Four').fadeOut(1000);
+  	$('#pFour').fadeOut();
+	$('#h1Four').fadeOut();
   }
 function hiddendescription5(){
-	$('#pFive').fadeOut(1000);
-	setTimeout('h1FiveFadeOut()',100);
- }
-function h1FiveFadeOut(){
-	$('#h1Five').fadeOut(1000);
+	$('#pFive').fadeOut();
+	$('#h1Five').fadeOut();
   }
 function occurdescription1(){
-	$('#h1One').fadeIn(1000);
-	setTimeout('pOneFadeIn()',100);
+	$('#h1One').fadeIn(500);
+	setTimeout('pOneFadeIn()',200);
   }
 function pOneFadeIn(){
-	$('#pOne').fadeIn(1000);
+	$('#pOne').fadeIn(500);
   }
 function occurdescription2(){
-	$('#h1Two').fadeIn(1000);
-	setTimeout('pTwoFadeIn()',100);
+	$('#h1Two').fadeIn(500);
+	setTimeout('pTwoFadeIn()',200);
   }
 function pTwoFadeIn(){
-	$('#pTwo').fadeIn(1000);
+	$('#pTwo').fadeIn(500);
   }
 function occurdescription3(){
-	$('#h1Three').fadeIn(1000);
-  setTimeout('pThreeFadeIn()',100);
+	$('#h1Three').fadeIn(500);
+  setTimeout('pThreeFadeIn()',200);
   }
 function pThreeFadeIn(){
-	$('#pThree').fadeIn(1000);
+	$('#pThree').fadeIn(500);
   }
 function occurdescription4(){
-	$('#h1Four').fadeIn(1000);
-	setTimeout('pFourFadeIn()',100);
+	$('#h1Four').fadeIn(500);
+	setTimeout('pFourFadeIn()',200);
   }
 function pFourFadeIn(){
-  $('#pFour').fadeIn(1000);
+  $('#pFour').fadeIn(500);
   }
 function occurdescription5(){
-	$('#h1Five').fadeIn(1000);
-  setTimeout('pFiveFadeIn()',100);
+	$('#h1Five').fadeIn(500);
+  setTimeout('pFiveFadeIn()',200);
   }
 function pFiveFadeIn(){
-  $('#pFive').fadeIn(1000);
+  $('#pFive').fadeIn(500);
   }
 function hiddendescription(){
 	$('#h1Two').fadeOut();
